dfd1c807fa5d405a9b346521440e6bec4928d9c1
Moaz Alla Elden <moaz.dev@yahoo.com>
2024-01-12 19:56:11 +0200

    :zap:

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import vue from "@vitejs/plugin-vue";
 export default defineNuxtConfig({  
+
+  plugins: [vue()],
+  base: "https://moaz-dev.github.io/Portfolio-2024/",
+  
   /*devtools: {
     enabled: true,
     timeline: {
