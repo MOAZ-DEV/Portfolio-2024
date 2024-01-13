5044befb4a3f8dea95162663c212a381911e84c6
Moaz Alla Elden <moaz.dev@yahoo.com>
2024-01-13 19:57:08 +0200

    :fire:

--- server/api/img/[url].ts
+++ server/api/img/[url].ts
@@ -1,22 +1,16 @@
 import list from "../../../public/projects.json";
 import {readFileSync} from "node:fs";
-import path from "node:path";
 
 export default
     defineEventHandler((event) => {
 
         let result: any,
-            filePath:string,
             CallName = getRouterParam(event, 'url');
 
         list.forEach(p =>
             p.img === CallName ? () => {
-                filePath = path.join(
-                    process.cwd(),
-                    'public/images/',
-                    `${CallName}.png`);
-                result = readFileSync(filePath);
-            } : null);
+                result = readFileSync(`/${CallName}.png`);
+            } : result = "There nothing! Stop scraping..");
 
         return result;
     })
