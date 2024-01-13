b171455c7f03f4abe2bf587d8fb892094f6f69a0
Moaz Alla Elden <moaz.dev@yahoo.com>
2024-01-13 11:52:11 +0200

    :zap:

--- server/api/img/[url].ts
+++ server/api/img/[url].ts
@@ -1,5 +1,6 @@
 import list from "../../../public/projects.json";
 import fs from "node:fs";
+import path from "node:path";
 
 export default
     defineEventHandler((event) => {
@@ -7,8 +8,12 @@ export default
         let result: any,
             CallName = getRouterParam(event, 'url');
 
-        list.forEach( p =>
-            p.img === CallName ? result = fs.readFileSync(`images/${CallName}`) : null);;
+        list.forEach(p =>
+            p.img === CallName ? () => {
+                const
+                    filePath = path.join(process.cwd(), 'public/images/', `${CallName}.png`);
+                result = fs.readFileSync(filePath);
+            } : null);
 
         return result;
     })
