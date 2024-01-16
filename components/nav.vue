6c85c4a9d00983e548d2924e1925b9eaa059c211
Moaz Alla Elden <moaz.dev@yahoo.com>
2024-01-16 14:08:52 +0200

    :zap:

--- components/nav.vue
+++ components/nav.vue
@@ -3,7 +3,7 @@
     <img class="logo" src="~/assets/logo.svg" alt="logo">
 
     <div class="menu-wrap">
-      <button class="action"><a href="~/assets/Moaaz - Resume.pdf">Resume</a></button>
+      <button class="action"><a href="~/assets/Moaaz - Resume.pdf" download="Moaaz-Resume.pdf">Resume</a></button>
       <button>FAQ</button>
     </div>
     <button class="menu-btn"><img src="~/assets/menu.svg" /></button>
