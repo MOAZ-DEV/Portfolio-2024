6e0c18bfe59ac1ef4911a11f3dd53bb6c4088a62
Moaz Alla Elden <moaz.dev@yahoo.com>
2024-01-28 13:50:02 +0200

    :(

--- components/nav.vue
+++ components/nav.vue
@@ -6,7 +6,7 @@
       <button class="action"><a href="/Moaaz - Resume.pdf" download>Resume</a></button>
       <button>FAQ</button>
     </div>
-    <button class="menu-btn"><a href="/Moaaz - Resume.pdf" download><img src="~/assets/open_file.svg" /></a></button>
+    <button class="menu-btn"><a href="/Moaaz - Resume.pdf" download><img src="~/assets/open_file.svg" alt="cv"/></a></button>
   </nav>
 </template>
 <style lang="scss" src="../styling/components.scss"></style>
